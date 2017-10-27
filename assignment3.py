edab55c0c6df63e8450ff3cce84b71665603a54d
Fran Ramunno <franramunno@gmail.com>
2017-10-27 14:16:01 -0700

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -7,18 +7,107 @@ from scipy.stats import binom
 def get_binomial(n,p):
     return binom.rvs(n,p)
 
-# function to return the index of the shortest queue
-def get_first_fit_server(server_array):
-    count = 1
-    shortest_index = 0
+# function to return the index of the server with the
+# shortest queue - to be used with first fit
+def get_first_fit_server(server_array,cpu):
+    count = 0
     while count < len(server_array):
-        if server_array[count] < server_array[shortest_index]:
-            shortest_index = count
+        if (server_array[count]+cpu <= 1):
+            return count
         count = count+1
-    return shortest_index
 
-# function to generate d based on given mu val
+# function to return the index of the server with the
+# best fit for the given CPU Val
+def get_best_fit_server(server_arr, job_size):
+    best_fit_index = 0
+    best_difference = 1 - (server_arr[0] + job_size)
+    # if the first best index doesnt have room make it the max, 1
+    if best_difference < 0:
+        best_difference = 1
+    for s in range(len(server_arr)):
+        difference = 1 - (server_arr[s] + job_size)
+        # we want to minimize the difference as much as possible
+        # without it getting to be less than 0
+        if (difference < best_difference) and (difference >= 0):
+            best_difference = difference
+            best_fit_index = s
+    return best_fit_index
+
+def assign_to_servers(vm_arrivals,server_arr,str):
+    for s in range(len(vm_arrivals)):
+
+        # -- assign to server with corresponding CPU requirements -- #
+        if vm_arrivals[s] == 1:
+            if str == "bf":
+                index = get_best_fit_server(server_arr,VM_CPUS[0])
+            if str == "ff":
+                index = get_first_fit_server(server_arr,VM_CPUS[0])
+            server_arr.insert(index, VM_CPUS[0])
+
+        if vm_arrivals[s] == 2:
+            if str == "bf":
+                index = get_best_fit_server(server_arr,VM_CPUS[1])
+            if str == "ff":
+                index = get_first_fit_server(server_arr,VM_CPUS[1])
+            server_arr.insert(index, VM_CPUS[1])
+
+        if vm_arrivals[s] == 3:
+            if str == "bf":
+                index = get_best_fit_server(server_arr,VM_CPUS[2])
+            if str == "ff":
+                index = get_first_fit_server(server_arr,VM_CPUS[2])
+            server_arr.insert(index, VM_CPUS[2])
+
+# function to do departures
+def do_departures(server_list,vm_types_list):
+    for s in range(T):
+        if server_list[s] != 0:
+            vm_types = vm_types_list[s]  # get the types of VMs the server has
+            # so [#type1,#type2,#type3]
+            # for each VM type generate a departure if it is non-zero
+            if vm_types[0] != 0:
+                for i in range(vm_types[0]):
+                    dep = generate_departure(VM_MEANS[0])
+                    if dep == 1:
+                        server_list[s] = server_list[s] - VM_CPU[0]
+                        to_change = vm_types_list[s]
+                        to_change[0] = to_change[0] - 1
+            if vm_types[1] != 0:
+                for i in range(vm_types[1]):
+                    dep = generate_departure(VM_MEANS[1])
+                    if dep == 1:
+                        server_list[s] = server_list[s] - VM_CPU[1]
+                        to_change = vm_types_list[s]
+                        to_change[1] = to_change[1] - 1
+            if vm_types[2] != 0:
+                for i in range(vm_types[2]):
+                    dep = generate_departure(VM_MEANS[2])
+                    if dep == 1:
+                        server_list[s] = server_list[s] - VM_CPU[2]
+                        to_change = vm_types_list[s]
+                        to_change[2] = to_change[2] - 1
+
+def get_server_count(server_arr):
+    count = 0
+    for s in range(len(server_arr)):
+        if s!= 0:
+            count = count + 1
+    return count
+
+def get_array_of_arrivals():
+    vm_arrivals = []
+    num_vms = get_binomial(int(n), LAMBDA)  # get the number of VMs that arrive as binomial r.v
+    # print("n= ", n, " num vms= ", num_vms)
+
+    # get the type of VMs that arrived, 1/3 probability of each
+    for s in range(num_vms):
+        vm_type = generate_vm_type()
+        vm_arrivals.append(vm_type)
+    return vm_arrivals
+
+# function to generate departure based on given mu val
 def generate_departure(mean):
+    mu_val = 1.0 / mean
     rand_num = random.uniform(0,1)
     if (rand_num >= mu_val):
         return 1
@@ -38,113 +127,81 @@ def generate_vm_type():
             else:
                 return 3
 
-T = 100000 # time slots
+T = 101 # time slots
 LAMBDA = 0.9 # lambda value
 
-server_list = [0] * T # holds the servers; 0 represents a server not in use
-vm_types_list = [0,0,0] * len(server_list) # works with server list, keeps track of the amount of each type the server has
+server_list_ff = [0] * T # holds the servers; 0 represents a server not in use
+vm_types_list_ff = [[0,0,0]] * len(server_list_ff) # works with server list, keeps track of the amount of each type the server has
 
-# -- 3 VMs with their CPU unit requirements and mean #
+server_list_bf = [0] * T
+vm_types_list_bf = [[0,0,0]] * len(server_list_bf)
+
+# -- 3 VMs with their CPU unit requirements and mean -- #
 VM_CPUS = [0.5,0.25,0.125]
 VM_MEANS = [10,8,30]
 
-time_slot = 0
 i = 2
 n = math.pow(2,i) # start with n = 2^2
 
+x_axis = []
+y_axis_ff = []
+y_axis_bf = []
+
 while n <= math.pow(2,5):
+    print(n)
+    # keep track of the servers to get the average for both
+    # best fit and first fit
+    server_count_ff = 0
+    server_count_bf = 0
+
+    avg_count_ff = 0
+    avg_count_bf = 0
+
+    time_slot = 0
+
     while time_slot < T:
 
         # -- GENERATE DEPARTURES FIRST -- #
-        for s in range(len(server_list)):
-            if server_list[s] != 0:
-                vm_types = vm_types_list[s] # get the types of VMs the server has
-                                            # so [#type1,#type2,#type3]
-
-                # for each VM type generate a departure if it is non-zero
-                for i in range(vm_types[0]):
-                    if i != 0:
-                        dep = generate_departure(VM_MEANS[0])
-                        if dep == 1:
-                            server_list[s] = server_list[s] - VM_CPU[0]
-                            to_change = vm_types_list[s]
-                            to_change[0] = to_change[0] - 1
-
-                for i in range(vm_types[1]):
-                    if i != 0:
-                        dep = generate_departure(VM_MEANS[1])
-                        if dep == 1:
-                            server_list[s] = server_list[s] - VM_CPU[1]
-                            to_change = vm_types_list[s]
-                            to_change[1] = to_change[1] - 1
-
-                for i in range(vm_types[2]):
-                    if i != 0:
-                        dep = generate_departure(VM_MEANS[2])
-                        if dep == 1:
-                            server_list[s] = server_list[s] - VM_CPU[2]
-                            to_change = vm_types_list[s]
-                            to_change[2] = to_change[2] - 1
+        do_departures(server_list_ff,vm_types_list_ff)
+        do_departures(server_list_bf,vm_types_list_bf)
 
         # -- GENERATE ARRIVALS -- #
-        vm_arrivals = []
-        num_vms = get_binomial(int(n), LAMBDA)  # get the number of VMs that arrive as binomial r.v
-        # print("n= ", n, " num vms= ", num_vms)
-
-        # get the type of VMs that arrived, 1/3 probability of each
-        for s in range(len(num_vms)):
-            vm_type = generate_vm_type()
-            vm_arrivals.append(vm_type)
+        arrivals_ff = get_array_of_arrivals()
+        arrivals_bf = get_array_of_arrivals()
 
         # -- ASSIGN ARRIVALS TO SERVERS -- #
+        assign_to_servers(arrivals_ff,server_list_ff,"ff")
+        assign_to_servers(arrivals_bf,server_list_bf,"bf")
+
+        # -- GET THE COUNTS OF BOTH FIRST FIT AND BEST FIT -- #
+        server_count_ff = server_count_ff + get_server_count(server_list_ff)
+        server_count_bf = server_count_bf + get_server_count(server_list_bf)
+
         time_slot = time_slot + 1
+
+    avg_count_bf = float(server_count_bf) / T
+    print(avg_count_bf)
+    avg_count_ff = float(server_count_ff) / T
+    print(avg_count_ff)
+    x_axis.append(n)
+    y_axis_bf.append(avg_count_bf)
+    y_axis_ff.append(avg_count_ff)
+
     i = i + 1
     n = math.pow(2,i)
 
-## best fit function -- so far--- i think i should quit computing
-# science because sometimes i can't figure out very simple things :( :(
-# hope it will work
-def best_fit_algorithm(server,jobs):
-    server_size = len(server)
-    job_size = len(jobs)
- #Stores block id of the block allocated to a process
-    m=0
-    k=0
-    best_fit_array = []
-#Initially no block is assigned to any process
-    for k in range (len(best_fit_array)):
-        best_fit_array [k] = -1
- #   pick each process and find suitable blocks according to its size ad assign to it
-    for i in jobs:
-        #Find the best fit block for current process
-        index = -1
-        k = k+1
-        for j in  server:
-            if (server[m]>=jobs[k]):
-                if (index == -1):
-                    index = j
-                elif (server[index]>server[m]):
-                    index = j
-        m = m+1
-        #If we could find a block for current process
-        if (index !=-1):
-            #allocate block j to best_fit_array[i] process
-            best_fit_array [i] = index
-            #Reduce available memory in this server.
-            server[index] -= jobs[i]
-        
-    for i in job_size:
-        if (best_fit_array[i] != -1):
-            best_fit_array = best_fit_array[i] + 1
-    return best_fit_array
-             
+plt.xlabel("N")
+plt.ylabel("Average number of servers")
+plt.title("Average number of servers vs. N for best fit and first fit policies")
+plt.legend(('Best fit policy', 'First fit policy'), loc='upper left')
 
-server = [233,3333,333,333,33]
-jobs = [233,222,2222,22,0]
+plt.grid(True)
 
-a=best_fit_algorithm(server,jobs)
+plt.plot(x_axis, y_axis_bf)
+plt.plot(x_axis, y_axis_ff)
 
-print(a)
+plt.show()
+
 
 
 
